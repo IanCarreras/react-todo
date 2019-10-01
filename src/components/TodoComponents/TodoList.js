887d6091e7b5d5d4575f564bfcb9c8ab423680b5
Ian <mus5002@aol.com>
2019-10-01 19:18:48 -0400

    created todo and todolist components, initialized placeholder state for app.js

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,17 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react'
+
+import Todo from './Todo'
+
+export default function TodoList({ todos }) {
+    return (
+        <div>
+            {
+                todos.map(todo => {
+                    return <Todo task={todo.task} />
+                })
+            }
+        </div>
+    )
+}
